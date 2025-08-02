08457b9004195f810917da12caaba141178befd9
nob1as <46359314+nobias1@users.noreply.github.com>
2025-08-02 15:15:44 -0400

    Add files via upload

---
+++ crypto_course.py
@@ -0,0 +1,140 @@
+import base64
+
+# Converting numbers to ASCII using the chr() function
+asciiNums = [99, 114, 121, 112, 116, 111, 123, 65, 83, 67, 73, 73, 95, 112, 114, 49, 110, 116, 52, 98, 108, 51, 125]
+for i in asciiNums:
+    print(chr(i))
+
+# Converting hexString to Bytes
+hexString = '72bca9b68fc16ac7beeb8f849dca1d8a783e8acf9679bf9269f7bf'
+
+fromHex = bytes.fromhex(hexString)
+print("Original hex string converted to bytes: " + str(fromHex) + '\n')
+
+# Converting Bytes string back to Hex
+fromHexToBytes = bytes.hex(fromHex)
+print("Bytes string converted back to hex: " + str(fromHexToBytes))
+print(str(hexString) + '\n')
+
+# Checking if both strings match
+if str(fromHexToBytes) == hexString:
+    print("Both strings match. Conversion successful" + '\n')
+else:
+    print("The 2 strings do not match" + '\n')
+
+# Now we have to convert fromhexToBytes to Base64
+fromBytestoB64 = base64.b64encode(fromHex)
+print(fromBytestoB64)
+
+
+'''
+CryptoHacks Explanation for converting messages to numbers as seen in the RSA Public-Key Cryptography Standards:
+
+How should we convert our messages into numbers so that mathematical operations can be applied?
+The most common way is to take the ordinal bytes of the message, convert them into hexadecimal, and concatenate. This can be interpreted as a base-16/hexadecimal number, and also represented in base-10/decimal.
+
+My understanding:
+RSA uses Octet String to Integer Primitive or "OS2IP". This algorithm converts strings to numbers in the following way.
+    1. First consider "HELLO", as a sequence of numerical byte values: [72, 69, 76, 76, 79].
+    2. Then, these byte values are "glued" together in the following manner:
+        The calculation is:
+(72 * 256⁴) + (69 * 256³) + (76 * 256²) + (76 * 256¹) + (79 * 256⁰)
+
+Let's do the math:
+72 * 4,294,967,296 = 309,237,645,312
+
+69 * 16,777,216 = 1,157,627,904
+
+76 * 65,536 = 4,980,736
+
+76 * 256 = 19,456
+
+79 * 1 = 79
+Adding them all up:
+309,237,645,312 + 1,157,627,904 + 4,980,736 + 19,456 + 79 = 310,400,273,487
+
+This is done with PyCryptodomes bytes_to_long() function. The reverse is done with long _to_bytes().
+
+The Reverse Process: Division and Remainder
+While bytes_to_long() uses multiplication and addition, long_to_bytes() uses their opposites: division and remainder (modulo). It repeatedly divides the large number by 256 and records the remainder at each step to reconstruct the original bytes.
+
+Here’s how it works on our big number, 310400273487:
+
+Step 1:
+310400273487 % 256 gives a remainder of 79 (the byte for 'O').
+The number becomes 310400273487 // 256 = 1212501068.
+
+Step 2:
+1212501068 % 256 gives a remainder of 76 (the byte for 'L').
+The number becomes 1212501068 // 256 = 4736332.
+
+Step 3:
+4736332 % 256 gives a remainder of 76 ('L').
+The number becomes 4736332 // 256 = 18501.
+
+Step 4:
+18501 % 256 gives a remainder of 69 ('E').
+The number becomes 18501 // 256 = 72.
+
+Step 5:
+72 % 256 gives a remainder of 72 ('H').
+The number becomes 72 // 256 = 0. The process stops.
+
+The function collects these remainders ([79, 76, 76, 69, 72]) and reverses them to get the original big-endian sequence: [72, 69, 76, 76, 79], which is the message "HELLO".
+'''
+
+from Crypto.Util.number import *
+
+# bytes_to_long() to convert a message to numbers
+# long_to_bytes() to do the reverse
+
+revstring = long_to_bytes(11515195063862318899931685488813747395775516287289682636499965282714637259206269)
+print(revstring)
+
+# XOR Starter
+# https://cryptohack.org/courses/intro/xor0/
+# XOR is a bitwise operator which returns 0 if the bits are the same, and 1 otherwise. In textbooks the XOR operator is denoted by ⊕, but in most challenges and programming languages you will see the caret ^ used instead
+
+xorString = 'label'
+xorStringChars = list(xorString)
+print(xorStringChars)
+
+# Convert list of characters to list of Unicode values using ord() function
+unicodeConversion = []
+counter = 0
+for i in xorStringChars:
+    print(ord(i))
+    unicodeConversion.insert(counter, ord(i))
+    counter += 1 
+#    print(counter)
+
+print(unicodeConversion)
+
+# XOR each value in unicodeConversion
+convertNewValsToString = []
+for i in unicodeConversion:
+    print(i ^ 13)
+    newval = i ^ 13
+    convertNewValsToString.insert(counter, newval)
+    counter += 1
+print(convertNewValsToString)
+
+# Convert list of Unicode values back to Strings using chr() function
+
+for i in convertNewValsToString:
+    print(chr(i))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
